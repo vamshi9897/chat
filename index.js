b1e340f67aac07a6a52e58beab580364cfc5b202
vamshi9897 <vamshibathula9897@gmail.com>
2024-10-15 16:48:14 +0530

    updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { use } = require('express/lib/application');
 const http = require('http');
 const socketIo = require('socket.io');
 
@@ -7,41 +8,50 @@ const server = http.createServer(app);
 const io = socketIo(server); // Attach Socket.io to the server
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // Serve the HTML file
+  res.sendFile(__dirname + '/index.html'); // Serve the HTML file
 });
 
-const users = {};
+const users={}
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
 
-io.on('connection', socket => {
-    console.log('User connected:', socket.id);
+//   // Emit "joined" event when a new user joins
+//   socket.on('new', (name) => {
+//     users[socket.id]=name
+//     console.log(name,"joined")
+//     socket.broadcast.emit('joined', name);
+//   });
 
-    // Handle new user joining
-    socket.on('new', name => {
-        users[socket.id] = name; // Store user's name
-        console.log(name + " joined");
-        socket.broadcast.emit('joined', name); // Notify others
-    });
+//   // Listen for messages from clients
+//   socket.on('message', (message) => {
+//     io.emit('response', message); // Broadcast message to all clients
+//   });
 
-    // Handle messages from users
-    socket.on('message', message => {
-        if (message) { // Check if message is not empty
-            io.emit('response', { message: message, name: users[socket.id] }); // Broadcast message with sender's name
-        }
-    });
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
 
-    // Handle user disconnecting
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-        // Remove user from the users object
-        const userName = users[socket.id];
-        if (userName) {
-            delete users[socket.id];
-            socket.broadcast.emit('joined', userName + " left the chat"); // Notify others about the disconnection
-        }
-    });
-});
 
-// Start the server
+io.on('connection',socket=>{
+    console.log('user conected')
+
+    socket.on('new',name=>{
+        users[socket.id]=name
+        console.log(name,"joined")
+        socket.broadcast.emit('joined',name)
+    })
+
+    socket.on('message' ,message=>{
+        io.emit('response',{message:message,name:users[socket.id]})
+    })
+
+    socket.on('disconnect',()=>{
+        console.log(`user disconnected`)
+        delete users[socket.id]
+    })
+})
+
 server.listen(8080, () => {
-    console.log('Listening on 8080');
+  console.log('listening on *:8080');
 });
